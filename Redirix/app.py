36a950c0a8663bf9637c361b22533ca87aee9e91
Swaraj <swaraj.s.khandai.009@gmail.com>
2026-04-23 23:47:21 +0530

    updated app.run for deployment

--- Redirix/app.py
+++ Redirix/app.py
@@ -101,6 +101,5 @@ def analyze():
 
     except Exception as e:
         return jsonify({'error': f'An unexpected error occurred: {str(e)}'}), 500
-
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run()
